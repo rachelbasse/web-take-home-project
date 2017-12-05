7f4e454901d3b65ae98091f5511b4249d61e2fe6
Sam Henderson <hendersonsam456@gmail.com>
2017-12-06 10:16:21 +1100

    Added PROJECT_TYPES & CONTRACT_VALUES to the Constants file. These constants are the options for the "Select Project Type" and "Select a contract value" dropdown fields.

--- src/util/Constants.js
+++ src/util/Constants.js
@@ -18,7 +18,63 @@ export const PRIVACY_TYPE_AUTH = 'auth';
 export const PRIVACY_TYPE_BASIC = 'basic';
 export const API_URL = "https://staging-api.workyard.com";
 
+// Add Project Modal
+export const PROJECT_TYPES = [
+  {
+    "id": "1",
+    "name": "Home Renovation"
+  },
+  {
+    "id": "2",
+    "name": "Granny Flat"
+  },
+  {
+    "id": "3",
+    "name": "New Home"
+  },
+  {
+    "id": "4",
+    "name": "Multi Unit Residential"
+  },
+  {
+    "id": "5",
+    "name": "High End Residential"
+  },
+  {
+    "id": "6",
+    "name": "Retail"
+  },
+  {
+    "id": "7",
+    "name": "Commercial"
+  }
+];
+
+export const CONTRACT_VALUES = [
+  {id: 0, description: '$500 — $1K', min: 500, max: 1000},
+  {id: 1, description: '$1K — $2.5K', min: 1000, max: 2500},
+  {id: 2, description: '$2.5K — $5K', min: 2500, max: 5000},
+  {id: 3, description: '$5K — $10K', min: 5000, max: 10000},
+  {id: 4, description: '$10K — $15K', min: 10000, max: 15000},
+  {id: 5, description: '$15K — $20K', min: 15000, max: 20000},
+  {id: 6, description: '$20K — $30K', min: 20000, max: 30000},
+  {id: 7, description: '$30K — $40K', min: 30000, max: 40000},
+  {id: 8, description: '$40K — $50K', min: 40000, max: 50000},
+  {id: 9, description: '$50K — $75K', min: 50000, max: 75000},
+  {id: 10, description: '$75K — $100K', min: 75000, max: 100000},
+  {id: 11, description: '$100K — $200K', min: 100000, max: 200000},
+  {id: 12, description: '$200K — $300K', min: 200000, max: 300000},
+  {id: 13, description: '$400K — $500K', min: 400000, max: 500000},
+  {id: 14, description: '$500K — $1M', min: 500000, max: 1000000},
+  {id: 15, description: '$1M — $2M', min: 1000000, max: 2000000},
+  {id: 16, description: '$2M — $3M', min: 2000000, max: 3000000},
+  {id: 17, description: '$3M — $4M', min: 3000000, max: 4000000},
+  {id: 18, description: '$4M — $5M+', min: 4000000, max: 5000000}
+];
 
 //-----------------------------------------------
 //---------------- Action names -----------------
 //-----------------------------------------------
+
+// Store you action names e.g. ADD_PROJECT, here
+
